c98678b24ac33436d39f2419bb21af602b1d4d60
hliebovkyrylo <kirilglebov06@gmail.com>
2025-07-10 18:55:09 +0300

    refactor: added cache to get contaminated point

--- apps/api/src/contaminated-point/use-cases/get-contaminated-point/get-contaminated-point.use-case.ts
+++ apps/api/src/contaminated-point/use-cases/get-contaminated-point/get-contaminated-point.use-case.ts
@@ -2,19 +2,31 @@ import {
   HttpException,
   Inject,
   InternalServerErrorException,
+  Logger,
   NotFoundException,
 } from '@nestjs/common';
-import { IContaminatedPointRepository } from '@ecorally/dal';
+import { ContaminatedPoint, IContaminatedPointRepository, RedisService } from '@ecorally/dal';
 import { GetContaminatedPointCommand } from './get-contaminated-point.command';
 
 export class GetContaminatedPointUseCase {
+  private readonly logger = new Logger(GetContaminatedPointUseCase.name);
+  private readonly CACHE_TTL = 300;
+
   constructor(
     @Inject('CONTAMINATED_POINT_REPOSITORY')
     private readonly contaminatedPointRepository: IContaminatedPointRepository,
+    private readonly redis: RedisService,
   ) {}
 
   async execute(command: GetContaminatedPointCommand) {
     try {
+      const cacheKey = `contaminated-point:${command.contaminatedPointId}`;
+      const cachedPoint = await this.redis.get(cacheKey);
+      if (cachedPoint) {
+        this.logger.log(`Cache hit for contaminated point ${command.contaminatedPointId}`);
+        return JSON.parse(cachedPoint) as ContaminatedPoint;
+      }
+
       const contaminatedPoint = await this.contaminatedPointRepository.getById(
         command.contaminatedPointId,
       );
@@ -24,6 +36,9 @@ export class GetContaminatedPointUseCase {
           `Contaminated point with ID ${command.contaminatedPointId} not found`,
         );
       }
+
+      await this.redis.set(cacheKey, JSON.stringify(contaminatedPoint), this.CACHE_TTL);
+      this.logger.log(`Cache was set for contaminated point ${command.contaminatedPointId}`);
 
       return contaminatedPoint;
     } catch (error: unknown) {
